580ad3c22e059944001d026692805e2c66bb8757
RHRiday <riffatriday@gmail.com>
2021-12-26 13:40:33 +0600

    database connection

---
+++ BlazorApp1/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+    }
+}
